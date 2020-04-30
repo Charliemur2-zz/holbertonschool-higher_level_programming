49416fad492212b018c48ad8b496fa3dee3ddc0e
Charliemur2 <charliemurciamunoz@gmail.com>
2020-04-30 22:11:02 +0000

    create 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv
+    i = 1
     if (len(argv) == 1):
         print("0 arguments.")
     elif (len(argv) == 2):
@@ -9,7 +10,6 @@ if __name__ == "__main__":
         print("{}: {}".format(i, argv[i]))
     else:
         print("{} arguments:".format(len(argv) - 1))
-        i = 1
         while (i < len(argv)):
             print("{}: {}".format(i, argv[i]))
             i += 1
