07b0fae629de6a23ba9cf7f0ffa5422b08262b0a
Charliemur2 <charliemurciamunoz@gmail.com>
2020-05-08 05:02:08 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     if type(roman_string) is not str or not roman_string:
         return (0)
     roman_nums = dict([('I', 1), ('V', 5), ('X', 10), ('L', 50),
-                  ('C', 100), ('D', 500), ('M', 1000)])
+                       ('C', 100), ('D', 500), ('M', 1000)])
     number_int = 0
     for rom_num1, rom_num2 in zip(roman_string, roman_string[1:]):
         if roman_nums[rom_num1] < roman_nums[rom_num2]:
