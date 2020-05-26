739cf195b36fca7cf478ac98f6747b66fed78e40
Charliemur2 <charliemurciamunoz@gmail.com>
2020-05-26 02:22:06 +0000

    create 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
@@ -87,4 +87,4 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(self.size, self.size)
+        return cls(size, size)
