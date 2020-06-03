cca6de03f04c486e3c1c966d84d5fd43bd650473
Charliemur2 <charliemurciamunoz@gmail.com>
2020-06-03 22:10:09 +0000

    create 2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -10,9 +10,9 @@ def read_lines(filename="", nb_lines=0):
     """
     with open(filename, encoding="utf-8") as the_file:
         lines = the_file.readlines()
-        if nb_lines <= 0 or nb_lines >= len(lines):
-            for i in lines:
-                print(i, end=" ")
-        else:
-            for i in range(nb_lines):
-                print(lines[i], end=" ")
+    if nb_lines <= 0 or nb_lines >= len(lines):
+        for i in lines:
+            print(i, end=" ")
+    else:
+        for i in range(nb_lines):
+            print(lines[i], end=" ")
