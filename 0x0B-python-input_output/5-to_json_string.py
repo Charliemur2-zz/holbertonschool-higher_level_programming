a06f51ab99ba3e1060542fa26a685efcbd022925
Charliemur2 <charliemurciamunoz@gmail.com>
2020-06-03 22:02:43 +0000

    create 5-to_json_string.py

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import json
 """
-unction that returns the JSON representation of an object (string)
+function that returns the JSON representation of an object (string)
 """
 
 
