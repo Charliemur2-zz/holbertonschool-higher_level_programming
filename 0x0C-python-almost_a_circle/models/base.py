1f29270ef478d179efc603d716cc6883eae2e44d
Charliemur2 <charliemurciamunoz@gmail.com>
2020-06-09 18:06:26 +0000

    fix empty string base.py for 18 y 19 task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -78,7 +78,7 @@ class Base:
         returns a list of instances
         """
         file_name = cls.__name__ + ".json"
-        json.obj = []
+        json_obj = []
         try:
             with open(file_name, "r") as f:
                 json_obj = cls.from_json_string(f.read())
