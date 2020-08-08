623d32db32a0286ecece0387b9fe524e81298d1a
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-08 16:07:04 +0000

    create 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,12 @@ import sys
 arg = sys.argv
 
 if __name__ == "__main__":
-        db = MySQLdb.connect(host="localhost", user=arg[1], passwd=arg[2], db=arg[3], port=3306)
-        cur = db.cursor()
-        names = cur.execute("SELECT * FROM states ORDER BY states.id")
-        names = cur.fetchall
-        for item in names:
-            print(item)
-        db.close()
+    if len(arg) > 4:
+        print("Usage: ./0-select_states.py username password database")
+    db = MySQLdb.connect(host="localhost", user=arg[1], passwd=arg[2], db=arg[3], port=3306)
+    cur = db.cursor()
+    names = cur.execute("SELECT * FROM states ORDER BY states.id")
+    names = cur.fetchall()
+    for item in names:
+        print(item)
+    db.close()
