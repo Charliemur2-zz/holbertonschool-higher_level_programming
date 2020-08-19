e52fd5408126a5339c37b5d38dfc5c542019170e
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-19 00:57:23 +0000

    create 6-peak.py  6-peak.txt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,7 +5,11 @@ function that returns the peak of a list
 
 
 def find_peak(list_of_integers):
-    if list_of_integers > 0:
+    """
+    Find the peak in list_of_integers
+    Returns: Peak
+    """
+    if list_of_integers:
         list_of_integers.sort()
         return list_of_integers[-1]
     else:
