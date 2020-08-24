789b9d0c4c038e4988d74b8c2e85d3474df45bd6
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-24 22:21:24 +0000

    create 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    answer = 'Body response:\n\t- type: {}\n\t- content: {}'\
+        .format(type(r.text), r.text)
+    print(answer)
