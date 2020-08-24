b6fbd8bdc4e1f64f23b7f12b09649b410b10cf59
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-24 21:29:19 +0000

    create 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,6 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     email = {"email": argv[2]}
-    
     data = urllib.parse.urlencode(email)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
