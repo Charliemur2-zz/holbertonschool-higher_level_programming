ebd835d7c0291e127f34b9e93ee8553f0d669d68
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-15 22:43:23 +0000

    create 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2) {
-  console.log('Not a number');
-} else if (parseInt(process.argv[2])) {
+if (parseInt(process.argv[2])) {
   console.log('My number is: ' + (parseInt(process.argv[2])));
 } else {
   console.log('Not a number');
