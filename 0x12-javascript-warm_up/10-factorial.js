9949a95f9cf381840753893cd73bbf9332be9fa7
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-16 02:53:41 +0000

    create 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function factorial (n) {
-    if (n === 0) {
-      return (1);
-    }
-    return (n * factorial(n - 1));
+  if (n === 0) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
 }
 
 if (!parseInt(process.argv[2])) {
