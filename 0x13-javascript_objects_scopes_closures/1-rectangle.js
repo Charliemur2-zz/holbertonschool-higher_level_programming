d50cb9885dea728d9574e5fcd4f25020a8ab32aa
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-21 18:46:43 +0000

    create 1-main.js  1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle
+
+class Rectangle {
+  // create a class Square
+  constructor (w, h) {
+    // constructor for pass arguments in Rectangle class
+    this.height = h;
+    this.width = w;
+  }
+}
+module.exports = Rectangle;
