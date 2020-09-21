2ee85243fa941b4effefe565b6498fd8ed58d3a9
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-21 19:40:45 +0000

    create 2-main.js 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-class Rectangle {                                                                                         
-  constructor (w, h) {
+class Rectangle {
+  constructor (w, y) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
