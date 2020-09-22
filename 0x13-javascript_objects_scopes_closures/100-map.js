1a36a91dacd2ce2528499d7db84c6bd0a2a58ac4
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-22 04:59:09 +0000

    create 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-let procesList = list.map(x => x * list.indexOf(x));
+const procesList = list.map(x => x * list.indexOf(x));
 console.log(list);
 console.log(procesList);
