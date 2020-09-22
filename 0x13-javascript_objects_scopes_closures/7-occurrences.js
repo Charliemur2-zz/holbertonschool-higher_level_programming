ccf532f935404bd33d5c01c9d80a303a159d0121
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-22 01:50:43 +0000

    create 7-main.js 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-exports.nbOccurences = function(list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  list.forEach(function(list) {
+  list.forEach(function (list) {
     if (list === searchElement) {
       count += 1;
     }
