1e92b7f5d2637ef5eea97e239650ff36757a2f29
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-22 22:34:21 +0000

    create 0-readme.js README.md

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
 const file = require('fs');
-try {
-  const data = file.readFileSync(argv[2], 'utf8');
+file.readFile(argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
   console.log(data);
-} catch (err) {
-  console.error(err);
-}
+});
