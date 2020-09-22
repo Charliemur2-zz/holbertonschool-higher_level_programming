a267a42c2f3338f2b2eb62b73cca0f236bbad261
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-22 23:23:41 +0000

    create 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ request(argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
