ed2a78b2b160323198e58933a33d02c8e9829f83
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-22 00:49:10 +0000

    create 6-main.js 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,9 +5,9 @@ class Square extends square {
   charPrint(c) {
     if (c) {
       for (let i = 1; i <= this.width; i++) {
-      console.log(c.repeat(this.width));
+        console.log(c.repeat(this.width));
     } else {
-      this.print();
+        this.print();
     }
   }
 }
