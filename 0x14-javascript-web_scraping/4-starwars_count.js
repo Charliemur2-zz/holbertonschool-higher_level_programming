ab85a76f1acc59c06d5601da26819c8a52a061b0
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-23 01:09:25 +0000

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const argv = process.argv;
+const url = argv[2];
+const charUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+const request = require('request');
+let numMovies = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const resultList = JSON.parse(body).results;
+    resultList.forEach(function (charactersList) {
+      charactersList.characters.forEach(function (character) {
+        if (character === charUrl) {
+          numMovies++;
+        }
+      });
+    });
+    console.log(numMovies);
+  }
+});
